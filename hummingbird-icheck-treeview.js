261eb0fefde072f3fae7563a335901abd8f21d2a
bodyjar <bodyjar@hotmail.com>
2017-11-06 13:18:51 -0500

    Update hummingbird-icheck-treeview.js

--- hummingbird-icheck-treeview.js
+++ hummingbird-icheck-treeview.js
@@ -448,9 +448,16 @@
 		    }
 	    	}else{
 			var all_sum = $(this).siblings().addBack().children("label").children("div").children('input:checkbox').length;
-			if(all_sum == checked_unchecked_sum){
-				$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('determinate');
-				$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('check');
+			if(state){
+				if(all_sum == checked_unchecked_sum){
+					$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('determinate');
+					$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('uncheck');
+				}
+			}else{
+				if(all_sum == checked_unchecked_sum){
+					$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('determinate');
+					$(this).parent().parent().children("label").children("div").children("input[type='checkbox']").iCheck('check');
+				}
 			}
 		}
 
